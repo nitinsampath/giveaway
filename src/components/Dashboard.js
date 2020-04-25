bcecd5199e30ca2aff46337a4441c001a8d186ae
nitinsampath <nitfremont@gmail.com>
2020-04-24 23:39:27 -0700

    render list of raffles on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 
-export default class Dashboard extends React.Component {
+class Dashboard extends React.Component {
+    
 
+    // probably need to move the conditional render of raffles to a different component called RaffleList
     render() {
+        console.log(this.props.raffles); 
         return (
             <div>
-                Dashboard
+                <h1>Dashboard</h1>
+                <button onClick={() => {this.props.history.push("/raffle/new")}}>Create Giveaway</button>
+                <p>Active Giveaways</p>
+                {this.props.raffles && 
+                    this.props.raffles.map((raffle) => {
+                    return (
+                        <div key={raffle.id}>
+                            <p>{raffle.raffleName}</p>
+                            <p>{raffle.raffleDescription}</p>
+                            ----------------------------
+                        </div>
+                        )
+                    })}
             </div>
         )
     }
 
 }
+
+const mapStateToProps = (state) => {
+    return {
+        raffles: state
+        
+    };
+};
+
+export default connect(mapStateToProps)(Dashboard);
